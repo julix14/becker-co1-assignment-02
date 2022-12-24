40f072b11f8664395f4ddcbe6da24f8e2a7a1dc8
julix14 <julix.beker@gmail.com>
2022-12-24 14:47:29 +0100

    Add location setup

--- src/EventPlanner.java
+++ src/EventPlanner.java
@@ -1,6 +1,9 @@
 import classes.Location;
 import classes.event.Event;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class EventPlanner {
     private Location[] locations;
     private final String[] FUNCTIONS = {
@@ -13,12 +16,29 @@ public class EventPlanner {
             "Show the least used location",
     };
 
+    private final Scanner scanner = new Scanner(System.in);
+
     public EventPlanner(int locationCount){
         locations = new Location[locationCount];
     }
 
     public void runPlanner(){
+        this.locations = setupLocations();
+        System.out.println(Arrays.toString(this.locations));
+
         //While loop with MenuHandlerRun Call and switch case with return value
+    }
+
+    private Location[] setupLocations(){
+        System.out.printf("%nPlease enter the name and max capacity of %d locations:", this.locations.length);
+        for (int i = 0; i < locations.length; i++) {
+            System.out.printf("%nPlease enter the name of location %d: ", i+1);
+            String name = scanner.next();
+            System.out.printf("Please enter the maximum capacity of location %d: ", i+1);
+            int maxCapacity = scanner.nextInt();
+            locations[i] = new Location(name, maxCapacity);
+        }
+        return locations;
     }
 
     private void createNewEvent(){
--- src/Starter.java
+++ src/Starter.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Starter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //Example
-        //new EventPlanner(5).runPlanner();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello on the event planner!");
+        System.out.print("For how many locations do you want to plan events? ");
+        int locationCount = scanner.nextInt();
+
+        new EventPlanner(locationCount).runPlanner();
     }
 
 
